550507b3da73013e341b1962e7a707507dc4d8ff
zrx <731935354@qq.com>
2017-10-01 13:10:10 +0800

    Greedy O(n)

--- 45_JumpGameII.java
+++ 45_JumpGameII.java
@@ -1,21 +1,14 @@
-// TLE
 class Solution {
     public int jump(int[] nums) {
-        int len = nums.length;
-        int[] minStep = new int[len];
-        minStep[0] = 0;
-        for(int i = 1; i < len; i++)
-            minStep[i] = -1;
-        for(int i = 0; i < len-1; i++) {
-            for(int j = 1; j <= nums[i]; j++) {
-                if(i + j < len) {
-                    if(minStep[i+j] == -1) 
-                        minStep[i+j] = minStep[i] + 1;
-                    else if(minStep[i] + 1 < minStep[i + j])
-                        minStep[i + j] = minStep[i] + 1;
-                }
+        if(nums.length == 1) return 0;
+        int curBegin = 0, curEnd = 0, curMax = 0, step = 0;
+        for(int i = 0; i < nums.length-1; i++) {
+            curMax = Math.max(nums[i]+i, curMax);
+            if(i == curEnd) {
+                step++;
+                curEnd = curMax;
             }
         }
-        return minStep[len - 1];
+        return step;
     }
 }
